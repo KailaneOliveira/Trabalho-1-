c3a9a045e85c5b52400aa02a9cacb8a3cca390df
KailaneOliveira <kkailaneoliveiras@gmail.com>
2025-05-19 19:30:03 -0300

    Delete Trabalho directory

--- Trabalho/trabalhoj.js
+++
@@ -1,6 +0,0 @@
-const botao = document.getElementById('botaomensagem');
-const mensagem = document.getElementById('Obrigada por visitar este trabalho!');
-
-botao.addEventListener('click', function() {
-  mensagem.textContent = "Obrigada por visitar este trabalho!";
-});
